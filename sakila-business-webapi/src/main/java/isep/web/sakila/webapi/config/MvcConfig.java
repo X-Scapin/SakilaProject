7753f3a123e5615a0ea4354328ce9c22bc23475d
votre_pseudo <moi@email.com>
2016-02-03 20:13:34 +0100

    simple connexion(securite statique) pseudo=user et mdp=password

--- sakila-business-webapi/src/main/java/isep/web/sakila/webapi/config/MvcConfig.java
+++ sakila-business-webapi/src/main/java/isep/web/sakila/webapi/config/MvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import isep.web.sakila.jpa.config.PersistenceConfig;
@@ -12,6 +13,14 @@ import isep.web.sakila.jpa.config.PersistenceConfig;
 @EnableAutoConfiguration
 @ComponentScan(basePackages = { "isep.web.sakila.webapi" })
 @Import({ PersistenceConfig.class })
+
 public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+    }
 
 }
