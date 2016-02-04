5e18e00826b9edcda97d8436f81bda2732ef45d7
X-Scapin <prossato@isep.fr>
2016-02-04 19:40:47 +0100

    Delete film considering contraints

--- sakila-business-webapi/src/main/java/isep/web/sakila/webapi/service/FilmServiceImpl.java
+++ sakila-business-webapi/src/main/java/isep/web/sakila/webapi/service/FilmServiceImpl.java
@@ -14,14 +14,12 @@ import isep.web.sakila.dao.repositories.FilmActorRepository;
 import isep.web.sakila.dao.repositories.FilmCategoryRepository;
 import isep.web.sakila.dao.repositories.FilmRepository;
 import isep.web.sakila.dao.repositories.LanguageRepository;
-import isep.web.sakila.jpa.entities.Address;
 import isep.web.sakila.jpa.entities.Film;
 import isep.web.sakila.jpa.entities.FilmActor;
 import isep.web.sakila.jpa.entities.FilmCategory;
+import isep.web.sakila.jpa.entities.Inventory;
 import isep.web.sakila.jpa.entities.Language;
-import isep.web.sakila.jpa.entities.Store;
 import isep.web.sakila.webapi.model.FilmWO;
-import isep.web.sakila.webapi.utils.Util;
 
 @Service("filmService")
 @Transactional
@@ -31,12 +29,15 @@ public class FilmServiceImpl implements FilmService {
 	
 	@Autowired
 	private LanguageRepository languageRepository;
-
+	
+	@Autowired
+	private FilmActorRepository filmActorRepository;
+	
 	@Autowired
 	private FilmCategoryRepository filmCategoryRepository;
 	
 	@Autowired
-	private FilmActorRepository filmActorRepository;
+	private InventoryService inventoryService;
 	
 	private static final Log log = LogFactory.getLog(FilmServiceImpl.class);
 
@@ -91,6 +92,15 @@ public class FilmServiceImpl implements FilmService {
 	}
 	
 	public void deleteFilmById(int id) {
+		Film film = filmRepository.findOne(id);
+		Iterable<FilmActor> actors = film.getFilmActors();
+		Iterable<FilmCategory> categories = film.getFilmCategories();
+		List<Inventory> inventories = film.getInventories();
+		filmActorRepository.delete(actors);
+		filmCategoryRepository.delete(categories);
+		for (Inventory i : inventories) {
+			inventoryService.deleteInventoryById(i.getInventoryId());
+		}
 		filmRepository.delete(id);
 	}
 }
