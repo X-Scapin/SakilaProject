31e03bd3167edd200784a271b50a12802271d417
X-Scapin <prossato@isep.fr>
2016-02-04 16:36:46 +0100

    Fix resources bug for templates

--- sakila-business-webapi/src/main/java/isep/web/sakila/webapi/WebSecurityConfig.java
+++ sakila-business-webapi/src/main/java/isep/web/sakila/webapi/WebSecurityConfig.java
@@ -16,8 +16,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 
-		http.authorizeRequests().antMatchers("/resources/**").permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+		http
+			.authorizeRequests()
+				.antMatchers("/resources/**")
+					.permitAll() //.anyRequest().authenticated()
+					.and()
+						.formLogin()
+							.loginPage("/login").permitAll().and().logout().permitAll();
 	}
 
 	@Autowired
